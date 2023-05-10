cd4a8d190bfb9eb52b25c22fa126e8d6a0b2e115
jeromeRoyan <jerome.royan@b-com.com>
2023-05-10 10:57:43 +0200

    Auto stash before merge of "feature/UpgradeStaticDependencies" and "origin/feature/UpgradeStaticDependencies"

--- tests/run.sh
+++ tests/run.sh
@@ -7,11 +7,13 @@ ld_library_path="./"
 for modulePath in $(grep -o "\$XPCF_MODULE_ROOT.*lib" $1_conf.xml)
 do
    modulePath=${modulePath/"\$XPCF_MODULE_ROOT"/${XPCF_MODULE_ROOT}}
-   if ! [[ $ld_library_path =~ "$modulePath/x86_64/shared/release" ]]
+   if ! [[ $ld_library_path =~ "$modulePath/x86_64/shared/debug" ]]
    then
-      ld_library_path=$ld_library_path:$modulePath/x86_64/shared/release
+      ld_library_path=$ld_library_path:$modulePath/x86_64/shared/debug
    fi 
 done
 
 echo "LD_LIBRARY_PATH=$ld_library_path $1"
 LD_LIBRARY_PATH=$ld_library_path $1
+
+
